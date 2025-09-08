0a75ba40b681732f5778727919014b2e87c38d24
Axel Taldir <ataldir@cisco.com>
2025-09-08 15:06:48 +0200

    fix(hub): do not display apikey secret anymore (#370)
    Signed-off-by: Axel Taldir <ataldir@cisco.com>

--- hub/cmd/apikey/options/delete.go
+++ hub/cmd/apikey/options/delete.go
@@ -5,14 +5,25 @@ package options
 
 import (
 	"github.com/agntcy/dir/hub/cmd/options"
+	"github.com/spf13/cobra"
 )
 
 type APIKeyDeleteOptions struct {
 	*options.HubOptions
+
+	JSONOutput bool
 }
 
-func NewAPIKeyDeleteOptions(hubOptions *options.HubOptions) *APIKeyDeleteOptions {
-	return &APIKeyDeleteOptions{
+func NewAPIKeyDeleteOptions(hubOptions *options.HubOptions, cmd *cobra.Command) *APIKeyDeleteOptions {
+	opt := &APIKeyDeleteOptions{
 		HubOptions: hubOptions,
 	}
+
+	opt.AddRegisterFn(func() error {
+		cmd.Flags().BoolVarP(&opt.JSONOutput, "json", "j", false, "Output in JSON format")
+
+		return nil
+	})
+
+	return opt
 }
