9c591084ede0e7fe220b86310f74aed0feaefad2
Luca Muscariello <muscariello@ieee.org>
2025-09-11 20:26:12 +0200

    chore: add SPDX and copyright headers to Go bindings
    Signed-off-by: Luca Muscariello <muscariello@ieee.org>

--- blockchain/dapps/bindings/go/agentstore/agentstore.go
+++ blockchain/dapps/bindings/go/agentstore/agentstore.go
@@ -1,3 +1,5 @@
+// Copyright AGNTCY Contributors (https://github.com/agntcy)
+// SPDX-License-Identifier: Apache-2.0
 // Code generated - DO NOT EDIT.
 // This file is a generated binding and any manual changes will be lost.
 
