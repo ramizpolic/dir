467ec374bd4c3e84353372c4cf30104617107934
Luca Muscariello <muscariello@ieee.org>
2025-09-12 10:42:42 +0200

    fix(go): resolve all lint and formatting issues in client and tests
    Signed-off-by: Luca Muscariello <muscariello@ieee.org>

--- blockchain/dapps/contracts/Counter.t.sol
+++ blockchain/dapps/contracts/Counter.t.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
+// Copyright AGNTCY Contributors (https://github.com/agntcy)
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.28;
 
 import {Counter} from "./Counter.sol";
